5f662a82da328896aeb15d7b824826acf73ade4b
Cory Nelson <fullnelsontech@gmail.com>
2022-12-04 19:23:32 -0500

    final touches for main piece and board

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const canvas = document.getElementById("board")
 const ctx = canvas.getContext("2d")
+const canvasNext = document.getElementById("next")
+const ctxNext = canvas.getContext("2d")
 
-let board = new Board()
-
-function play() {
-  board.reset()
-  let piece = new Piece(ctx)
-  piece.draw()
-
-  board.piece = piece
+let accountValues = {
+  score: 0,
+  level: 0,
+  lines: 0,
 }
+
+function updateAccount(key, value) {
+  let element = getElementById(key)
+  if (element) {
+    element.textContent = value
+  }
+}
+
+let account = new Proxy(accountValues, {
+  set: (target, key, value) => {
+    target[key] = value
+    updateAccount(key, value)
+    return true
+  },
+})
+
+let requestId
 
 const moves = {
   [KEY.LEFT]: (p) => ({ ...p, x: p.x - 1 }),
@@ -19,22 +34,98 @@ const moves = {
   [KEY.UP]: (p) => board.rotate(p),
 }
 
-document.addEventListener("keydown", (event) => {
-  if (moves[event.keyCode]) {
-    event.preventDefault()
+let board = new Board(ctx, ctxNext)
+addEventListener()
+initNext()
 
-    let p = moves[event.keyCode](board.piece)
+function initNext() {
+  ctxNext.canvas.width = 4 * BLOCK_SIZE
+  ctxNext.canvas.height = 4 * BLOCK_SIZE
+  ctxNext.scale(BLOCK_SIZE, BLOCK_SIZE)
+}
 
-    if (event.keyCode === KEY.SPACE) {
-      while (board.valid(p)) {
-        board.piece.move(p)
-        p = moves[KEY.DOWN](board.piece)
-      }
-    } else if (board.valid(p)) {
-      board.piece.move(p)
+function addEventListener() {
+  document.addEventListener("keydown", (event) => {
+    if (event.keyCode === KEY.P) {
+      pause()
     }
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    if (event.keyCode === KEY.ESC) {
+      gameOver()
+    } else if (moves[event.keyCode]) {
+      event.preventDefault()
 
-    board.piece.draw()
+      let p = moves[event.keyCode](board.piece)
+
+      if (event.keyCode === KEY.SPACE) {
+        while (board.valid(p)) {
+          account.score += POINTS.HARD_DROP
+          board.piece.move(p)
+          p = moves[KEY.DOWN](board.piece)
+        }
+      } else if (board.valid(p)) {
+        board.piece.move(p)
+        if (event.keyCode === KEY.DOWN) {
+          account.score += POINTS.SOFT_DROP
+        }
+      }
+      // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+
+      // board.piece.draw()
+    }
+  })
+}
+
+function resetGame() {
+  account.score = 0
+  account.lines = 0
+  account.level = 0
+  board.reset()
+  time = { start: 0, elapsed: 0, level: LEVEL[account.level] }
+}
+
+function play() {
+  resetGame()
+  time.start = performance.now()
+  if (requestId) {
+    cancelAnimationFrame(requestId)
   }
-})
+
+  animate()
+}
+
+function animate() {
+  time.elapsed = now - time.start
+  if (time.elapsed > time.level) {
+    time.start = now
+    if (!board.drop()) {
+      gameOver()
+      return
+    }
+  }
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+  board.draw()
+  requestId = requestAnimationFrame(animate)
+}
+
+function gameOver() {
+  cancelAnimationFrame(requestId)
+  ctx.fillStyle = "black"
+  ctx.fillRect(1, 3, 8, 1.2)
+  ctx.font = "1px Arial"
+  ctx.fillStyle = "red"
+  ctx.fillText("Game Over", 1.8, 4)
+}
+
+function pause() {
+  if (!requestId) {
+    animate()
+    return
+  }
+  cancelAnimationFrame(requestId)
+  requestId = null
+  ctx.fillStyle = "black"
+  ctx.fillRect(1, 3, 8, 1.2)
+  ctx.font = "1px Arial"
+  ctx.fillStyle = "yellow"
+  ctx.fillText("PAUSED", 3, 4)
+}
