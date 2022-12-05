14c64d7e21721d5fd7b49db044a0a87e08977553
Cory Nelson <fullnelsontech@gmail.com>
2022-12-05 14:57:28 -0500

    first attemtpt to draw border

--- piece.js
+++ piece.js
@@ -3,6 +3,7 @@ class Piece {
   y
   color
   shape
+  border
   ctx
   typeId
 
@@ -15,16 +16,20 @@ class Piece {
     this.typeId = this.randomizeTetrominoType(COLORS.length - 1)
     this.shape = SHAPES[this.typeId]
     this.color = COLORS[this.typeId]
+    this.border = BORDER[this.typeId]
     this.x = 0
     this.y = 0
   }
 
   draw() {
     this.ctx.fillStyle = this.color
+    this.ctx.styleRect = this.border
+
     this.shape.forEach((row, y) => {
       row.forEach((value, x) => {
         if (value > 0) {
-          this.ctx.fillRect(this.x + x, this.y + y, 1, 1)
+          this.ctx.fillRect(this.x + x, this.y + y, 1, 1) &&
+            this.ctx.strokeStyle(this.x + x, this.y + y, 1, 1)
         }
       })
     })
